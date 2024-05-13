1d98745768b85bc3611a6e27ef4da11e3cc43308
Marshal <mpetry2@hotmail.com>
2024-05-13 20:28:51 -0300

    building image

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ScrollSection from '@/components/ScrollSection'
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <ScrollSection />
+    </main>
+  );
+}
